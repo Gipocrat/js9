200f298f3144cacb934e3ac91526f4c79789d7f2
Gipocrat <163756857+Gipocrat@users.noreply.github.com>
2025-09-28 19:58:28 +0300

    сделал несколько фич

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ else {
 function createProduct(product) {
     counter +=1
     localStorage.setItem('counter', counter)
-    localStorage.setItem(`counter${counter}`, JSON.stringify({img:product.img, rating: product.rating, value: product.value, name: product.name, cost: product.cost}))
+    localStorage.setItem(`card${counter}`, JSON.stringify({img:product.img, rating: product.rating, value: product.value, name: product.name, cost: product.cost, id: counter}))
 }
 createProduct({img:'./img/2.webp', rating: 4.94, value: "500ккал", name: "Стейк из индейки охлаждённый Зелёная линия, 500г", cost: 450})
 createProduct({img:'./img/1.webp', rating: 4.84, value: "100ккал", name: "Томаты черри на ветке, 250г", cost: 120})
@@ -24,6 +24,7 @@ function appendProducts() {
         let newProduct = document.createElement("div")
         newProduct.className = "products__card"
         newProduct.innerHTML = `
+            <img class="card__delete" src="../img/delete.svg" alt="" data=${card.id}>
               <div class="card__image-block">
                   <img class="card__image" src='${card.img}' alt="">
                   </div>
@@ -42,4 +43,57 @@ function appendProducts() {
       }
     }
 }
+let imgAdd = document.querySelector(".img__add")
+let formFields = document.querySelector(".form__fields")
+imgAdd.addEventListener("click", () => {
+  imgAdd.classList.add("hide")
+  formFields.classList.add("show")
+});
+function sendInfo(event) {
+  event.preventDefault()
+  let imagePath = document.querySelector("#image-path").value
+  let rating = document.querySelector("#rating").value
+  let value = document.querySelector("#value").value
+  let name = document.querySelector("#name").value
+  let cost = document.querySelector("#cost").value
+  let addForm = document.querySelector(".add__form")
+  if (addForm.checkValidity()) {
+    createProduct({img:imagePath, rating: rating, value: value, name: name, cost: cost})
+    location.reload()
+  }
+  else {
+    console.log("Кажется, ничего не произошло!")
+  }
+}
+let formButton = document.querySelector(".form__button")
+formButton.addEventListener("click", function(event) {
+  sendInfo(event)
+});
 appendProducts()
+let cardDelete = document.querySelectorAll(".card__delete")
+let modal = document.querySelector(".modal")
+function addActiveClass() {
+  modal.classList.add("active")
+  modal.classList.remove("closed")
+}
+for (let i = 0; i < cardDelete.length; i++){  
+  let buttonDelete = cardDelete[i]
+  buttonDelete.addEventListener("click", function()  {
+      addActiveClass()  
+      let no = document.querySelector("#no")
+      no.addEventListener("click", () => {
+        modal.classList.remove("active")
+        modal.classList.add("closed")
+      });
+      let yes = document.querySelector("#yes")
+      yes.addEventListener("click", () => {
+        modal.classList.remove("active")
+        modal.classList.add("closed")
+        let card = `card${buttonDelete.getAttribute('data')}`
+        localStorage.removeItem(card)
+        setTimeout(()=> {
+          location.reload()
+        },600)
+      });                         
+  });
+}
